3e397525a16a1af5f491a7c830e454b89ae714c1
riabhatia <rmbhatia@calpoly.edu>
2025-06-01 18:24:19 -0700

    first commit

---
+++ data.py
@@ -0,0 +1,52 @@
+class Movie:
+    # Initialize a new Movie object.
+    # input: the movie's title as a string
+    # input: the movie's genre as a string
+    # input: the movie's duration as a float
+    # input: the movie's rating as a float
+    # input: the movie's leading actors as a list of strings
+    # input: the movie's director as a string
+    # input: the movie's racial demographics as a dictionary
+    # input: the movie's language as a string
+    # input: the movie's release year as an integer
+    # input: the movie's sales by continent as a dictionary
+
+    def __init__(self,
+                 title: str,
+                 genre: str,
+                 duration: float,
+                 rating: float,
+                 actors: list[str],
+                 director: str,
+                 racial_demographics: dict[str, int],
+                 language: str,
+                 release_year: int,
+                 sales:dict[str, int],):
+
+        self.title = title
+        self.genre = genre
+        self.duration = duration
+        self.rating = rating
+        self.actors = actors
+        self.director = director
+        self.racial_demographics = racial_demographics
+        self.language = language
+        self.release_year = release_year
+        self.sales = sales
+
+
+    # Provide a developer-friendly string representation of the object.
+    # input: Movie for which a string representation is desired.
+    # output: string representation
+    def __repr__(self):
+        return 'Movie({}, {}, {}, {}, {}, {}, {}, {}, {}, {})'.format(
+                self.title,
+                self.genre,
+                self.duration,
+                self.rating,
+                self.actors,
+                self.director,
+                self.racial_demographics,
+                self.language,
+                self.release_year,
+                self.sales)
---
+++ functions.py
@@ -0,0 +1,23 @@
+# This file has all the different functions we will be implementing for our project.
+
+# This function searches if a movie is on the platform
+# str argument is the string title representation of the movie
+# The function returns true if the movie is present and false if it is not present
+# def search_movie(string:str) -> bool:
+
+# This function evaluates if certain movies fall above a certain threshold and creates a new list of those movies who do
+# list argument is the list of various movie titles
+# float argument is the threshold that is being compared
+# This function returns a list of movies whose ratings fall above a certain threshold
+# def movies_above_rating(movies: list[Movie], threshold: float) -> list:
+
+# This function sorts movie titles alphabetically
+# list argument is the list of various movie titles
+# This function returns a list of movies in alphabetical order
+# def sort_alpha(movies: list[Movie]) -> list:
+
+# This function evaluates the average ticket sales for a particular region.
+# list argument is the list of various movie titles
+# string argument is the name of the continent being compared
+# This function returns the average sales a movie received in a continent
+# def average_sales_by_continent(movies: list[Movie], continent: str) -> float:

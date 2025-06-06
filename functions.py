e5f896dcd418a1abc491ab8ed1a30d10e56f39a8
caroline.b4 <cbohney@calpoly.edu>
2025-06-06 12:38:43 -0700

    second commit

--- functions.py
+++ functions.py
@@ -47,10 +47,10 @@ def search_movie(title: str, filename: str = "movies.txt") -> bool:
 #This function sorts through the movies and return those with subtitles available
 #input: a list of movies and a keyword "has subtitles"
 #output: a list of movies
-def accessible(movies: list[data.Movie]) -> list[data.Movie]:
+def accessible(movies: list[data.Movie], sub: str) -> list[data.Movie]:
     movies_list = []
     for movie in movies:
-        subtitle = movie.subtitles.get("En")
+        subtitle = movie.subtitles.get(sub)
         if subtitle in movie:
             movies_list.append(movie)
     return movies_list
@@ -62,7 +62,7 @@ def accessible(movies: list[data.Movie]) -> list[data.Movie]:
 def children_movies(movies: list[data.Movie]) -> list[data.Movie]:
     sorted_list = []
     for movie in movies:
-        if movie.maturity.get("G") or movie.maturity.get("PG"):
+        if movie.maturity == "G" or movie.maturity == "PG":
             sorted_list.append(movie)
     return sorted_list
 
@@ -73,12 +73,23 @@ def children_movies(movies: list[data.Movie]) -> list[data.Movie]:
 def young_adult_movies(movies: list[data.Movie]) -> list[data.Movie]:
     sorted_list = []
     for movie in movies:
-        if movie.maturity.get("G") or movie.maturity.get("PG") or movie.maturity.get("PG-13"):
+        if movie.maturity =="G" or movie.maturity == "PG" or movie.maturity =="PG-13":
             sorted_list.append(movie)
     return sorted_list
 
 #This function sorts the movies by genre and alphabetically
 #input: a list of movies and a genre
 #output a sorted list of movies of a single genre
-def sorted_by_genre(movies: list[data.Movie], type: str) -> list[data.Movie]:
+def sorted_by_genre(movies: list[data.Movie], genre_type: str) -> list[data.Movie]:
+   sorted_list = []
+   for movie in movies:
+       if movie.genre == genre_type: #sorts through only the movies of a certain genre
+           sorted_list.append(movie) #creates a list of the movies of a certain genre
+   for i in range(len(sorted_list)-1):
+       smallest_idx = i
+       for j in range(i + 1, len(sorted_list)):
+           if sorted_list[j].title < sorted_list[smallest_idx].title:
+               smallest_idx = j
+       sorted_list[i], sorted_list[smallest_idx] = sorted_list[smallest_idx], sorted_list[i]
+   return sorted_list
 
